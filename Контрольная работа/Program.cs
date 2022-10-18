fe3d23460bc67b17ac6e924653252f917f6d0d9a
Dubok <dlyapokupokspb@yandex.ru>
2022-10-19 02:33:27 +0300

    добавил файл решения задачи

---
+++ Контрольная работа/Program.cs
@@ -0,0 +1,37 @@
+﻿void ElementArray(string[] mass, int length)
+{
+    int i = 0;
+    while (i < length)
+    {
+        Console.WriteLine("Введите элемент массива: ");
+        mass[i] = Console.ReadLine();
+        i++;
+    }
+    Console.WriteLine($"Все элементы массива: {String.Join(", ", mass)}");
+}
+
+void SortArray(string[] array, int lengthArray, int maxLengthElement)
+{
+    int i = 0;
+    int lengthElement = 0;
+    while (i < lengthArray)
+    {
+        lengthElement = array[i].Length;
+        if (lengthElement <= maxLengthElement)
+        {
+            Console.Write($"{array[i]} ");
+            i++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+}
+
+Console.WriteLine("Введите количество элементов массива: ");
+int lengthArray = int.Parse(Console.ReadLine());
+int maxLengthElement = 3;
+string[] result = new string[lengthArray];
+ElementArray(result, lengthArray);
+SortArray(result, lengthArray, maxLengthElement);
